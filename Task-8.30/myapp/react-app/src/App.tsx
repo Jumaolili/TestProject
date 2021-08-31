8edc96d5441396b0af62035e950926f3012c4731
Jumaolili <601909441@qq.com>
2021-08-31 08:51:57 +0800

    update 8.31

--- Task-8.30/myapp/react-app/src/App.tsx
+++ Task-8.30/myapp/react-app/src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
+import './App.module.less';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+
+//引入页面组件
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <BrowserRouter>
+        {/* <Nav /> */}
+        <div className='container'>
+          {/* 根据URL路径匹配路由组件，渲染到该位置 */}
+          <Switch>
+            <Route path='/login' exact component={Login} />
+            <Route path='/register' exact component={Register} />
+            {/* <Route path='/addCapsule' component={} /> */}
+            {/* 这是一个默认页面，如果前面的路由都没有被匹配到，就会渲染这个组件，相当于404，一定要放到最后 */}
+            
+          </Switch>
+        </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Task-8.30/myapp/react-app/src/pages/Register/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './index.module.less';
+
+import RegistrationForm from '../../components/Form_Register'
+
+function Register() {
+  
+  return (
+    <div className={style.container_form_register}>
+      <img alt="知乎 LOGO" className={style.SignFlowHomepage_logo} src="https://pic2.zhimg.com/80/v2-f6b1f64a098b891b4ea1e3104b5b71f6_720w.png"/>
+        <div className={style.container_form_resgister_item}>
+          <RegistrationForm/>
+        </div>
+        <footer className="SignFlowHomepage-footer">
+            <div className="ZhihuLinks">
+              <a target="_blank" rel="noopener noreferrer" href="https://zhuanlan.zhihu.com">知乎专栏</a>
+              <a target="_blank" rel="noopener noreferrer" href="/roundtable">圆桌</a>
+              <a target="_blank" rel="noopener noreferrer" href="/explore">发现</a>
+              <a target="_blank" rel="noopener noreferrer" href="/app">移动应用</a>
+              <a target="_blank" rel="noopener noreferrer" href="/contact">联系我们</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://app.mokahr.com/apply/zhihu">来知乎工作</a>
+              <a target="_blank" rel="noopener noreferrer" href="/org/signup">注册机构号</a>
+              <a target="_blank" rel="noopener noreferrer" href="//ir.zhihu.com">Investor Relations</a>
+            </div>
+            <div className="ZhihuRights">
+              <span>© 2021 知乎</span>
+              <a target="_blank" rel="noopener noreferrer" href="https://tsm.miit.gov.cn/dxxzsp/">京 ICP 证 110745 号</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://beian.miit.gov.cn/">京 ICP 备 13052560 号 - 1</a>
+              <a target="_blank" rel="noopener noreferrer" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802020088">
+                <img alt="" src="https://pic3.zhimg.com/80/v2-d0289dc0a46fc5b15b3363ffa78cf6c7.png"/>京公网安备 11010802020088 号
+              </a>
+              <a href="https://zhstatic.zhihu.com/assets/zhihu/publish-license.jpg" target="_blank" rel="noopener noreferrer">出版物经营许可证</a>
+              <span>互联网药品信息服务资格证书（京）- 非经营性 - 2017 - 0067</span>
+            </div>
+            <div className="ZhihuReports">
+              <a target="_blank" rel="noopener noreferrer" href="https://zhuanlan.zhihu.com/p/28852607">侵权举报</a>
+              <a target="_blank" rel="noopener noreferrer" href="http://www.12377.cn">网上有害信息举报专区</a>
+              <a target="_blank" rel="noopener noreferrer" href="/term/child-jubao">儿童色情信息举报专区</a>
+              <span>违法和不良信息举报：010-82716601</span>
+            </div>
+          </footer>
+    </div>
+  );   
+}
+
+export default Register;
